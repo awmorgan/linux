f6de8b798e81e34c2c054c83c5b4f9cd471dedd4
Bill Morgan <arthurwilliammorgan@gmail.com>
2022-11-16 19:18:31 -0600

    add generated headers and config files

---
+++ include/generated/compile.h
@@ -0,0 +1,4 @@
+#define UTS_MACHINE		"aarch64"
+#define LINUX_COMPILE_BY	"arthurmorgan"
+#define LINUX_COMPILE_HOST	"Bills-MBP"
+#define LINUX_COMPILER		"Homebrew clang version 14.0.6, Homebrew LLD 14.0.6"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "6.1.0-rc5-00044-gcc675d22e422"
