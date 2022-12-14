d9765a021e89319659bab5c31e515fad705d1e56
Bill Morgan <arthurwilliammorgan@gmail.com>
2022-12-14 17:47:54 -0600

    start over

--- myusb/main.c
+++ myusb/main.c
@@ -1,13 +1,4 @@
 
-#undef __weak
-
-#define __KERNEL__
-#define MYBUILD
-
-#include <linux/compiler-version.h>
-#include <linux/kconfig.h>
-#include <linux/compiler_types.h>
-
 #include "../drivers/scsi/scsi.c"
 
 int main(int argc, char *argv[])
